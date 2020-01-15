d4ae2df5964e14b10f8eb644e8e10e78b7d44f48
Valéria Nicéria <valerianiceria@gmail.com>
2020-01-14 23:10:20 -0300

    backend

---
+++ backend/README.md
@@ -0,0 +1,6 @@
+### Métodos HTTP: get, post, put, delete
+
+#### Tipos de parâmetros: 
+- Query Params: request.query (Filtros, ordenação, paginação, ...)
+- Route Params: request.params (Identificar um recurso na alteração ou remoção)
+- Body: request.body (Dados para criação ou alteração de um registro)
---
+++ backend/src/controllers/DevController.js
@@ -0,0 +1,44 @@
+const axios = require('axios')
+const Dev = require('../models/Dev')
+
+// index, show, store, update, destroy
+
+module.exports = {
+
+    async index(request, response) {
+        const devs = await Dev.find()
+        return response.json(devs)
+    },
+
+    async store(request, response) {
+        const { github_username, techs, latitude, longitude } = request.body
+
+        // Verificando se o dev já está cadastrado
+        let dev = await Dev.findOne({ github_username })
+
+        if (!dev) {
+            const api_github = await axios.get(`https://api.github.com/users/${ github_username }`)
+            // Verifica se o nome existe, se não existir, utiliza o valor do login
+            const { name = login, bio, avatar_url } = api_github.data
+            const techsArray = techs.split(',').map(tech => tech.trim())
+        
+            const location = {
+                type: 'Point',
+                coordinates: [longitude, latitude]
+            }
+        
+            let dev = await Dev.create({
+                github_username,
+                name,
+                avatar_url,
+                bio,
+                techs: techsArray,
+                location
+            })
+        }
+
+       
+    
+        return response.json(dev)
+    }
+}
